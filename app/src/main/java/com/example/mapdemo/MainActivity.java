925e6a6e00316fe21e7f1a6d666ceb89415adb8d
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2020-08-13 14:41:19 +0530

    map commit

---
+++ app/src/main/java/com/example/mapdemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.mapdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        // Construct a GeoDataClient.
+        //mGeoDataClient = Places.getGeoDataClient(this, null);
+
+        // Construct a PlaceDetectionClient.
+        //mPlaceDetectionClient = Places.getPlaceDetectionClient(this, null);
+
+        // Construct a FusedLocationProviderClient.
+        //mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+    }
+}
